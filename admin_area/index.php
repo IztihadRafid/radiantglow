6db685d352a84e6a58611a6858249d50881e67b8
IztihadRafid <iztihadrafid10@gmail.com>
2024-03-17 15:19:19 +0600

    insert categories,insert brand from added, SQL created and connected succesfully

--- admin_area/index.php
+++ admin_area/index.php
@@ -9,6 +9,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- css file -->
     <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <style>
         .admin_image {
             width: 100px;
@@ -16,6 +17,22 @@
             border: 2px solid black;
             border-radius: 300px;
         }
+
+        .btn-admin {
+            background-color: black;
+            color: white;
+            border-radius: 15%;
+            padding-left: 12px;
+            padding-right: 12px;
+            padding-top: 8px;
+            padding-bottom: 8px;
+           
+        }
+
+        .btn-admin:hover {
+            background-color: #C7D1FF;
+            color: black;
+       }
     </style>
 </head>
 
@@ -49,24 +66,35 @@
                     <p class="text-light text-center">Admin Name</p>
                 </div>
                 <div class="button text-center mx-auto">
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">Insert Product</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">View Products</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">Insert Category</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">View Categories</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">Insert Brands</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">View Brands</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">All Orders</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">All Payments</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">List Users</a></button>
-                    <button class="my-2"><a href="" class="nav-link text-light bg-black p-2">Log Out</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">Insert Product</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">View Products</a></button>
+                    <button class="btn-admin my-2"><a href="index.php?insert_category" class="nav-link p-2">Insert Category</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">View Categories</a></button>
+                    <button class="btn-admin my-2"><a href="index.php?insert_brand" class="nav-link p-2">Insert Brands</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">View Brands</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">All Orders</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">All Payments</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">List Users</a></button>
+                    <button class="btn-admin my-2"><a href="" class="nav-link p-2">Log Out</a></button>
                 </div>
             </div>
         </div>
     </div>
 
+    <!-- GET insert category -->
+    <div class="container my-5">
+        <?php
+        if(isset($_GET['insert_category'])){
+            include('insert_categories.php');
+        }
+        if(isset($_GET['insert_brand'])){
+            include('insert_brands.php');
+        }
+        ?>
+    </div>
 
-        <!-- Footer -->
-    <div class="bg-black p-4 text-center">
+    <!-- Footer -->
+    <div class="bg-black p-4 text-center position-absolute bottom-0 w-100">
         <p class="text-white ">All rigths reserved , Designed By IRMN-2024</p>
     </div>
 
---
+++ admin_area/insert_brands.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Insert Brand</title>
+    <!-- bootstrap link -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <!-- css file -->
+    <link rel="stylesheet" href="../style.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+</head>
+
+<body>
+    <h1 class="text-center p-3">Insert Brands</h1>
+    <div class="border border-danger border-1 rounded-3 w-50 mx-auto mt-4 p-5">
+        <form action="" method="post" class="mb-2">
+            <div class="input-group mb-3 w-90">
+                <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
+                <input type="text" class="form-control" placeholder="Insert Brands" name="cat_title" placeholder="Insert Category" aria-label="Categories" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group w-10">
+
+                <!-- <input type="submit" class="form-control bg-info" value="Insert categories" name="insert_cat"> -->
+                <button class="bg-info py-3 px-5 my-3 border-0 rounded-2">Insert Brands</button>
+            </div>
+        </form>
+    </div>
+</body>
+
+</html>
