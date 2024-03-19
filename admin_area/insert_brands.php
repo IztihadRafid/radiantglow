9d71e846c97924acdd25d17a73f06dfe5b78bc12
IztihadRafid <iztihadrafid10@gmail.com>
2024-03-19 20:43:15 +0600

    Brand inserted in SQL

--- admin_area/insert_brands.php
+++ admin_area/insert_brands.php
@@ -13,17 +13,41 @@
 </head>
 
 <body>
+    <!-- PHP code  -->
+    <?php
+        include("../includes/connect.php");
+        // Create and Posting CATEGORY
+        if(isset($_POST["insert_brand"])){
+            $brand_title = $_POST["brand_title"];
+            
+            //select data from database
+            $select_query="Select * from `brands` where brand_title= '$brand_title'";
+            $result_select = mysqli_query($con, $select_query);
+            $number = mysqli_num_rows($result_select);
+            if($number>0){
+                echo "<script>alert('This Brand is present inside the database')</script>";
+            }
+            else{
+                $insert_query = "insert into `brands` (brand_title) values('$brand_title')";
+                $result = mysqli_query($con, $insert_query);
+                if($result){
+                    echo "<script>alert('Brand Has Been Inserted Successfully')</script>";
+            }
+            
+        }
+    }
+    ?>
     <h1 class="text-center p-3">Insert Brands</h1>
     <div class="border border-danger border-1 rounded-3 w-50 mx-auto mt-4 p-5">
         <form action="" method="post" class="mb-2">
             <div class="input-group mb-3 w-90">
                 <span class="input-group-text bg-info" id="basic-addon1"><i class="fa-solid fa-receipt"></i></span>
-                <input type="text" class="form-control" placeholder="Insert Brands" name="cat_title" placeholder="Insert Category" aria-label="Categories" aria-describedby="basic-addon1">
+                <input type="text" class="form-control" placeholder="Insert Brands" name="brand_title" placeholder="Insert Brands" aria-label="brands" aria-describedby="basic-addon1">
             </div>
             <div class="input-group w-10">
 
-                <!-- <input type="submit" class="form-control bg-info" value="Insert categories" name="insert_cat"> -->
-                <button class="bg-info py-3 px-5 my-3 border-0 rounded-2">Insert Brands</button>
+                <input type="submit" class="bg-info py-3 px-5 my-3 border-0 rounded-2" value="Insert Brands" name="insert_brand">
+               
             </div>
         </form>
     </div>
